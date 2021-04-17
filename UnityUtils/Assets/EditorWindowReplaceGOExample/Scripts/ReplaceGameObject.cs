89e294f8e544341b083e04b388a01f2fcbb8393a
Daniel Castaño Estrella <daniel.c.estrella@gmail.com>
2021-04-17 19:29:05 +0200

    replace game object by prefab example

---
+++ UnityUtils/Assets/EditorWindowReplaceGOExample/Scripts/ReplaceGameObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ReplaceGameObjectByPrefab : EditorWindow
+{
+    public GameObject prefab;
+
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Tools/ReplaceGameObjectByPrefab")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(ReplaceGameObjectByPrefab));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Replace selected GameObjects by prefab pressing the button.", EditorStyles.boldLabel);
+        prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), true);
+
+        if (GUILayout.Button("Replace Selected"))
+        {
+            if (!prefab) return;
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, obj.transform.parent);
+                if (!go) return;
+                go.transform.position = obj.transform.position;
+                go.transform.rotation = obj.transform.rotation;
+                go.transform.SetSiblingIndex(obj.transform.GetSiblingIndex());
+
+                DestroyImmediate(obj);
+            }
+        }
+    }
+}
