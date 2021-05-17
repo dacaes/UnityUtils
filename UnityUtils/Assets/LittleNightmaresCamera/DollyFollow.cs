bc1b787d66218b541904a99b203e2013275d5d8d
Daniel Castaño Estrella <daniel.c.estrella@gmail.com>
2021-05-17 09:54:01 +0200

    little nightmares camera and timeline example

---
+++ UnityUtils/Assets/LittleNightmaresCamera/DollyFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[ExecuteInEditMode]
+public class DollyFollow : MonoBehaviour
+{
+    public float xLength;
+    public Transform trackedObject;
+    public CinemachineDollyCart cart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cart.m_Position = trackedObject.position.x / xLength;
+    }
+}
